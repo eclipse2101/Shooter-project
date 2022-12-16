56fe53b4df9b32de01f2be25f1c5ba7faa62e5cf
eclipse2101 <rdericieux25@aoiths.org>
2022-12-16 09:11:30 -0500

    finish this at home

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    Rigidbody2D rigibody2D;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigibody2D = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Launch(Vector2 direction, float force) // this is to show where the cog is gonna go and how much force is gonna be applied to it
+    {
+        rigibody2D.AddForce(direction * force); 
+    }
+}
